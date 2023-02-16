270988b5a8d9e74ddcdff741d3d2ae7e80bc61d4
Endijs Dārznieks <endijs.darznieks@accenture.com>
2023-02-16 17:17:45 +0200

    Context version of the app

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,37 +1,36 @@
 // FOR CONTEXT
-// import { useContext } from 'react';
-// import { DataContext } from '../contexts/DataContext';
-
-// const useData = () => {
-//   const context = useContext(DataContext);
-
-//   if (!context) throw Error('useData() must be used inside DataProvider');
-
-//   return context;
-// };
-
-// FOR REDUX-TOOLKIT
-
-import { useSelector, useDispatch } from 'react-redux';
-import { UPDATE_DATA, UPDATE_SEARCH_INPUT } from '../redux/DataRedux';
+import { useContext } from 'react';
+import { DataContext } from '../contexts/DataContext';
 
 const useData = () => {
-  const redux = useSelector((state) => state.DataRedux);
-  const dispatch = useDispatch();
+  const context = useContext(DataContext);
 
-  if (!redux) throw Error('useData() must be used inside Provider');
+  if (!context) throw Error('useData() must be used inside DataProvider');
 
-  // FUNCTIONS
-  const fetchDataInApp = (data) => {
-    dispatch(UPDATE_DATA(data));
-  };
-
-  const updateSearchInput = (data) => {
-    dispatch(UPDATE_SEARCH_INPUT(data));
-  };
-
-  return { ...redux, fetchDataInApp, updateSearchInput };
+  return context;
 };
+
+// FOR REDUX-TOOLKIT
+// import { useSelector, useDispatch } from 'react-redux';
+// import { UPDATE_DATA, UPDATE_SEARCH_INPUT } from '../redux/DataRedux';
+
+// const useData = () => {
+//   const redux = useSelector((state) => state.DataRedux);
+//   const dispatch = useDispatch();
+
+//   if (!redux) throw Error('useData() must be used inside Provider');
+
+//   // FUNCTIONS
+//   const fetchDataInApp = (data) => {
+//     dispatch(UPDATE_DATA(data));
+//   };
+
+//   const updateSearchInput = (data) => {
+//     dispatch(UPDATE_SEARCH_INPUT(data));
+//   };
+
+//   return { ...redux, fetchDataInApp, updateSearchInput };
+// };
 
 // FOR BOTH EXPORT
 export default useData;
--- src/index.js
+++ src/index.js
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 // FOR CONTEXT
-// import { DataProvider } from './contexts/DataContext';
-import store from './redux/store';
-import { Provider } from 'react-redux';
+import { DataProvider } from './contexts/DataContext';
+// FOR REDUX-TOOLKIT
+// import store from './redux/store';
+// import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <DataProvider> */}
-    <Provider store={store}>
+    <DataProvider>
+      {/* <Provider store={store}> */}
       <App />
-    </Provider>
-    {/* </DataProvider> */}
+      {/* </Provider> */}
+    </DataProvider>
   </React.StrictMode>
 );
